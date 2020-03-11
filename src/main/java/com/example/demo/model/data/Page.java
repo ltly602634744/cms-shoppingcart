3b2d3c716915182d4664a1e75d7f7147ae4ff589
ltly602634744 <yan.liyan1204@gmail.com>
2020-03-11 19:37:16 -0400

    admin section
    add some new pages and relative controller
    
    Using fragment to reuse the html code

---
+++ src/main/java/com/example/demo/model/data/Page.java
@@ -0,0 +1,83 @@
+package com.example.demo.model.data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="pages")
+public class Page {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String title;
+	
+	private String slug;
+	
+	private String content;
+	
+	private int sorting;
+	
+	public Page() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	public Page(int id, String title, String slug, String content, int sorting) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.slug = slug;
+		this.content = content;
+		this.sorting = sorting;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSlug() {
+		return slug;
+	}
+
+	public void setSlug(String slug) {
+		this.slug = slug;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getSorting() {
+		return sorting;
+	}
+
+	public void setSorting(int sorting) {
+		this.sorting = sorting;
+	}
+	
+	
+}
