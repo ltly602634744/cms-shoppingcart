aef0d2cd6794996e2c6dc0c7bffa0df824a456e7
ltly602634744 <yan.liyan1204@gmail.com>
2020-03-13 21:10:36 -0400

    add POST Add page, test the validation and the duplication of the slug

--- src/main/java/com/example/demo/controller/AdminPagesController.java
+++ src/main/java/com/example/demo/controller/AdminPagesController.java
@@ -2,12 +2,17 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.model.PageRepository;
 import com.example.demo.model.data.Page;
@@ -40,6 +45,35 @@ public class AdminPagesController {
 		//model.addAttribute("page", new Page());
 		
 		return "admin/pages/add";
+	}
+	
+	@PostMapping("/add")
+	public String add(@Valid Page page, BindingResult bingBindingResult,
+						RedirectAttributes redirectAttributes, Model model) {
+		
+		if(bingBindingResult.hasErrors()) {
+			return "admin/pages/add";
+		}
+		
+		redirectAttributes.addFlashAttribute("message", "Page added");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+		
+		String slug = page.getSlug() == "" ? page.getTitle().toLowerCase().replace(" ", "-") : page.getSlug().toLowerCase().replace(" ", "-");
+		
+		Page slugExists = pageRepo.findBySlug(slug);
+		
+		if( slugExists != null ) {
+			redirectAttributes.addFlashAttribute("message", "Slug exists, choose another");
+			redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+			redirectAttributes.addFlashAttribute("page", page);
+		}else {
+			page.setSlug(slug);
+			page.setSorting(100);
+			
+			pageRepo.save(page);
+		}
+		
+		return "redirect:/admin/pages/add";
 	}
 }
 
--- src/main/java/com/example/demo/model/PageRepository.java
+++ src/main/java/com/example/demo/model/PageRepository.java
@@ -1,11 +1,10 @@
 package com.example.demo.model;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.data.Page;
 
 public interface PageRepository extends JpaRepository<Page, Integer> {
 	
+	Page findBySlug(String slug);
 }
