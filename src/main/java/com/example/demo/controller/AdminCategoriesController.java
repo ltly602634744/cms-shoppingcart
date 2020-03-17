6a4a1382b5ed9cd794fa7a06bc3e4d257ddd1881
ltly602634744 <yan.liyan1204@gmail.com>
2020-03-17 11:50:23 -0400

    Add the GET and POST category view and controller

--- src/main/java/com/example/demo/controller/AdminCategoriesController.java
+++ src/main/java/com/example/demo/controller/AdminCategoriesController.java
@@ -2,14 +2,21 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.model.CategoryRepository;
 import com.example.demo.model.data.Category;
+import com.example.demo.model.data.Page;
 
 @Controller
 @RequestMapping("/admin/categories")
@@ -28,6 +35,61 @@ public class AdminCategoriesController {
 		return "admin/categories/index";
 		
 	}
+	
+	/**
+	 * You can define this method to define the ModelAttribute for the 
+	 * whole file, if you define it, this java file can use this ModelAttribute
+	 * and you do not need to transfer the parameter into the methods
+	 * @return
+	 */
+//	@ModelAttribute("category")
+//	public Category getCategory() {
+//		return new Category();
+//	}
+	
+	/*
+	 * You can just delete the annotation @ModelAttribute, 
+	 * then the name of the variable must match the name 
+	 * in the add.html file, if you use @ModelAttribute annotation, 
+	 * you can change the name by yourself. 
+	 */
+	@GetMapping("/add")
+//	public String sdd(@ModelAttribute Category category) {
+//	public String sdd() {
+	public String sdd(Category category) {
+		return "admin/categories/add";
+	}
+	
+	@PostMapping("/add")
+	public String add(@Valid Category category, BindingResult bindingResult,
+						RedirectAttributes redirectAttributes, Model model) {
+		
+		if(bindingResult.hasErrors()) {
+			return "admin/categories/add";
+		}
+		
+		redirectAttributes.addFlashAttribute("message", "Category added");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+		
+		String slug = category.getName().toLowerCase().replace(" ", "-");
+		
+		Category categoryExists = categoryRepo.findByName(category.getName());
+		
+		if( categoryExists != null ) {
+			redirectAttributes.addFlashAttribute("message", "Category exists, choose another");
+			redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+			redirectAttributes.addFlashAttribute("categoryInfo", category);
+		}else {
+			category.setSlug(slug);
+			category.setSorting(100);
+			
+			categoryRepo.save(category);
+		}
+		
+		return "redirect:/admin/categories/add";
+	}
+	
+	
 }
 
 
--- src/main/java/com/example/demo/model/CategoryRepository.java
+++ src/main/java/com/example/demo/model/CategoryRepository.java
@@ -6,4 +6,6 @@ import com.example.demo.model.data.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer>{
 
+	Category findByName(String name);
+
 }
