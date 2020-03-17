0c2c13c62d047a9eb921028d07c83007e916cf01
ltly602634744 <yan.liyan1204@gmail.com>
2020-03-17 18:01:20 -0400

    Handle GET and POST edit a category

--- src/main/java/com/example/demo/controller/AdminCategoriesController.java
+++ src/main/java/com/example/demo/controller/AdminCategoriesController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -87,6 +88,46 @@ public class AdminCategoriesController {
 		}
 		
 		return "redirect:/admin/categories/add";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable int id, Model model) {
+		
+		Category category = categoryRepo.getOne(id);
+		
+		model.addAttribute("category", category);
+		
+		return "admin/categories/edit";
+	}
+	
+	@PostMapping("/edit")
+	public String edit(@Valid Category category, BindingResult bindingResult,
+						RedirectAttributes redirectAttributes, Model model) {
+		
+		Category categoryCurrent = categoryRepo.getOne(category.getId());
+		
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("pageTitle", categoryCurrent.getName());
+			return "admin/categories/edit";
+		}
+		
+		redirectAttributes.addFlashAttribute("message", "Category edited");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+		
+		String slug = category.getName().toLowerCase().replace(" ", "-");
+		
+		Category categoryExists = categoryRepo.findByName(category.getName());
+		
+		if( categoryExists != null ) {
+			redirectAttributes.addFlashAttribute("message", "Category exists, choose another");
+			redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+		}else {
+			category.setSlug(slug);
+			
+			categoryRepo.save(category);
+		}
+		
+		return "redirect:/admin/categories/edit/" + category.getId();
 	}
 	
 	
