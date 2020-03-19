c9824caf8c0bcb1e33ce0d38003d5d2425b798cf
ltly602634744 <yan.liyan1204@gmail.com>
2020-03-18 20:36:54 -0400

    GET add product and product entity validation

--- src/main/java/com/example/demo/controller/AdminProductsController.java
+++ src/main/java/com/example/demo/controller/AdminProductsController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.model.CategoryRepository;
 import com.example.demo.model.ProductRepository;
+import com.example.demo.model.data.Category;
+import com.example.demo.model.data.Page;
 import com.example.demo.model.data.Product;
 
 @Controller
@@ -16,16 +20,28 @@ import com.example.demo.model.data.Product;
 public class AdminProductsController {
 
 	@Autowired
-	private ProductRepository productRepository;
+	private ProductRepository productRepo;
 
+	@Autowired
+	private CategoryRepository categoryRepo;
+	
 	@GetMapping
 	public String index(Model model) {
 		
-		List<Product> products = this.productRepository.findAll();
+		List<Product> products = this.productRepo.findAll();
 		
 		model.addAttribute("products", products);
 		
 		return "/admin/products/index";
+	}
+	
+	@GetMapping("/add")
+	public String add(Product product, Model model) {
+		
+		List<Category> categories = categoryRepo.findAll();
+		model.addAttribute("categories", categories);
+		
+		return "admin/products/add";
 	}
 
 
