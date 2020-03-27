7aafedcbf07170259d40a8328ff57943c1dd0ae1
ltly602634744 <yan.liyan1204@gmail.com>
2020-03-27 09:59:07 -0400

    Spring security begin!
    Add the spring security dependency in pom.xml. Also, create the
    application yml file to set the user name and password. Create
    SecurityConfig class to set the access authority.

---
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.antMatchers("/**").hasAnyRole("USER");
+	}
+}
