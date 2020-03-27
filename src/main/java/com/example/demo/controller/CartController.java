4236ff3038a0bdaa1af71f709390d958394a1609
ltly602634744 <yan.liyan1204@gmail.com>
2020-03-26 21:19:43 -0400

    Add cart page
    the viewpage of cart, as well to add a product

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.model.Cart;
 import com.example.demo.model.ProductRepository;
@@ -25,7 +26,10 @@ public class CartController {
 	private ProductRepository productRepo;
 	
 	@GetMapping("/add/{id}")
-	public String add(@PathVariable int id, HttpSession session, Model model) {
+	public String add(@PathVariable int id, 
+						HttpSession session, 
+						Model model, 
+						@RequestParam(value = "cartPage", required = false) String cartPage) {
 		
 		Product product = productRepo.getOne(id);
 		
@@ -57,10 +61,27 @@ public class CartController {
 		model.addAttribute("size", size);
 		model.addAttribute("total", total);
 		
+		if (cartPage != null) {
+			return "redirect:/cart/view";
+		}
+		
 		
 		return "cart_view";
 	}
 	
+	
+	@RequestMapping("/view")
+	public String view(HttpSession session, Model model) {
+		if(session.getAttribute("cart") == null) {
+			return "redirect:/";
+		}
+		
+		Map<Integer, Cart> cart = (HashMap<Integer, Cart>)session.getAttribute("cart");
+		model.addAttribute("cart", cart);
+		model.addAttribute("notCartViewPage", true);
+		
+		return "cart";
+	}
 	
 	
 	
