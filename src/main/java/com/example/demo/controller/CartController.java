a8772702ea2e8a2545a74baa27ac8d573a965380
ltly602634744 <yan.liyan1204@gmail.com>
2020-03-27 00:02:45 -0400

    Add paypal checkout

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -93,6 +93,33 @@ public class CartController {
 		return "redirect:" + refererLink;
 	}
 	
+	@GetMapping("/remove/{id}")
+	public String remove(@PathVariable int id, HttpSession session, Model model,
+			HttpServletRequest httpServletRequest) {
+		
+		Map<Integer, Cart> cart = (HashMap<Integer, Cart>)session.getAttribute("cart");
+		
+		cart.remove(id);
+		if(cart.size() == 0) {
+			session.removeAttribute("cart");
+		}
+		
+		String refererLink = httpServletRequest.getHeader("referer");
+		
+		return "redirect:" + refererLink;
+	}
+	
+	@GetMapping("/clear")
+	public String clear(HttpSession session, HttpServletRequest httpServletRequest) {
+		
+		session.removeAttribute("cart");
+		
+		String refererLink = httpServletRequest.getHeader("referer");
+		
+		return "redirect:" + refererLink;
+	}
+	
+	
 	
 	@RequestMapping("/view")
 	public String view(HttpSession session, Model model) {
