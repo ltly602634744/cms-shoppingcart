6617bb84ae1ff22b22f714f4c0b7907392cd4c42
ltly602634744 <yan.liyan1204@gmail.com>
2020-03-27 21:29:05 -0400

    UserDetails Service

---
+++ src/main/java/com/example/demo/security/UserRepositoryUserDetailsServices.java
@@ -0,0 +1,50 @@
+package com.example.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.AdminRepository;
+import com.example.demo.model.UserRepository;
+import com.example.demo.model.data.Admin;
+import com.example.demo.model.data.User;
+
+@Service
+public class UserRepositoryUserDetailsServices implements UserDetailsService{
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Autowired
+	private AdminRepository adminRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = userRepo.findByUsername(username);
+		Admin admin = adminRepo.findByUsername(username);
+		
+		if (user != null) {
+			return user;
+		}
+		
+		if (admin != null) {
+			return admin;
+		}
+		
+		throw new UsernameNotFoundException("User: " + username + "notfound!");
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
