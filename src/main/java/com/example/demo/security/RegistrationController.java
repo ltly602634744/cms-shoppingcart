762745d669b9920805b6afaa5ca30fe56074268f
ltly602634744 <yan.liyan1204@gmail.com>
2020-03-27 20:34:54 -0400

    controller of the registration

--- src/main/java/com/example/demo/security/RegistrationController.java
+++ src/main/java/com/example/demo/security/RegistrationController.java
@@ -1,5 +1,33 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.UserRepository;
+
+@Controller
+@RequestMapping("/register")
 public class RegistrationController {
 
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
